d56d898afcc4e2b9800314d97a74bce729e406ad
clemsciences <clemsciences@users.noreply.github.com>
2025-06-11 22:43:59 +0000

    deploy: 6542e771572fd295e911851ab1111dbfc07448b1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3878403197"
+    serviceWorkerVersion: "1472200453"
   }
 });
